87462a9e286e022aa432e7fa83238df88fdf39b0
rbhogal <rbhogal23gmail.com>
2021-01-20 14:04:26 -0800

    added rendercurOperand func, other in progress

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-"use strict;";
+'use strict;';
 
 // Element Selectors
-const btnNumber = document.querySelector(".number__keys");
-const btnOperator = document.querySelector(".operator__keys");
-const btnEqual = document.querySelector("#equal");
-const curOperand = document.querySelector(".current__operand");
+const btnNumber = document.querySelector('.number__keys');
+const btnOperator = document.querySelector('.operator__keys');
+const btnEqual = document.querySelector('#equal');
+const btnDecimal = document.getElementById('decimal');
 
-const data = {
-  curOperand: [],
-  prevOperand: [],
-};
+const containerCurOperand = document.querySelector('.current__operand');
+const containerPrevOperand = document.querySelector('.previous__operand');
 
-// Event listeners
+const data = ['', ''];
+[curOperand, prevOperand] = data;
+
 // User inputs numbers
-btnNumber.addEventListener("click", function (e) {
+btnNumber.addEventListener('click', renderCurOperand.bind(this));
+
+// User hits an operator key
+btnOperator.addEventListener('click', renderPrevOperand.bind(this));
+
+// // User hits equals
+// btnEqual.addEventListener("click", storeInput.bind(this));
+
+// Event handlers (functions)
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Render input to calculator display as current operand
+function renderCurOperand(e) {
   // Add guard clause for ENTER (equal) button and div element
-  if (e.target.textContent === "=" || e.target.tagName === "DIV") return;
-  const number = e.target.textContent;
+  const click = e.target;
+  const input = e.target.textContent;
+
+  // Guard clauses for equal button and number__keys div
+  if (click.textContent === '=' || click.tagName === 'DIV') return;
 
   // 1) Render input to current operand and store it into data
+  containerCurOperand.textContent += input;
 
+  // 2) Store current operand input data into array
+  curOperand += input;
   console.log(curOperand);
-  render(number);
-});
 
-// User hits equals
-btnEqual.addEventListener("click", function (e) {
-  if (e.target.tagName === "DIV") return;
-  console.log(e.target.textContent);
+  // Decimal press only allowed once
+  if (click.textContent === '.') click.disabled = true;
+}
 
-  // Store current operand into data
+function renderPrevOperand(e) {
+  // Add guard clause for ENTER (equal) button and div element
+  const click = e.target;
+  const input = e.target.textContent;
 
-  // Display current operand into previous operand
+  // Guard clauses for equal button and number__keys div
+  if (click.textContent === 'DEL' || click.tagName === 'DIV') return;
+
+  // Add operator key text to previous operand display !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  //Display current operand into previous operand
+  containerPrevOperand.textContent = curOperand;
 
   // Clear the current operand
-});
-
-// Event handler functions
-function render(number) {
-  console.log(number);
-
-  // Display current input
-  curOperand.textContent += number;
+  clear();
+  console.log(curOperand);
+  // Convert curOperand to a number
+  // +curOperand
 }
+
+// function storeInput(e) {
+//   if (e.target.tagName === "DIV") return;
+//   console.log(e.target.textContent);
+
+//   // Display current operand into previous operand
+
+//   containerPrevOperand.textContent = curOperand;
+//   // Convert curOperand to a number
+//   // +curOperand
+
+//   // Clear the current operand
+// }
 
 // Clear current input display
 function clear() {
-  curOperand.textContent = "";
+  containerCurOperand.textContent = '';
 }
 
 // clear();
